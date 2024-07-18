90af714f3a7cda006c276a5743c99011995821e5
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-07-19 01:22:25 +0530

    feat: add service, model and pages

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,42 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WeatherService {
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final url = '$BASE_URL?q=$cityName&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCityWeather() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    String cityName = placemarks[0].locality!;
+    return cityName;
+  }
+}
